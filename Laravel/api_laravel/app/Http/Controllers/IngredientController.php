3fc2bcff55b0c20f7c8d01ff0230dc69262de943
Fullgaito <gaitansergioalejandrgmail.com>
2026-02-25 08:57:50 -0500

    Api crud con laravel

---
+++ Laravel/api_laravel/app/Http/Controllers/IngredientController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Ingredient;
+
+class IngredientController extends Controller
+{
+    public function index(){
+        $ingredients= Ingredient::all();
+        return response()->json($ingredients);
+    }
+    public function store(Request $request){
+        $ingredient=new Ingredient();
+        $ingredient->name=$request->name;
+        $ingredient->save();
+        return response()->json($ingredient);
+    }
+    public function update(Request $request,$id){
+        $ingredient=Ingredient::find($id);
+        $ingredient->name=$request->name;
+        $ingredient->save();
+        return response()->json($ingredient);
+    }
+    public function destroy($id){
+        $ingredient=Ingredient::find($id);
+        $ingredient->delete();
+        return response()->json("Eliminado");
+    }
+}
