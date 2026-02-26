9486f6828daefd8a726d5c6851e406b1c673ce98
Fullgaito <gaitansergioalejandrgmail.com>
2026-02-26 06:52:12 -0500

    Control de usuarios en Laravel y redireccionamiento y nueva direccion

--- Laravel/api_laravel/routes/api.php
+++ Laravel/api_laravel/routes/api.php
@@ -4,8 +4,34 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\IngredientController;
+use App\Http\Controllers\UserController;
+
+use Illuminate\Support\Facades\Http;
+
+
+/********** Rutas para el controlador de usuarios **********/
+Route::get("/users",[UserController::class, "index"]);
+Route::post("/users",[UserController::class, "store"]);
+Route::put("/users/{id}",[UserController::class, "update"]);
+Route::delete("/users/{id}",[UserController::class, "destroy"]);
+
 
 Route::get("/ingredients",[IngredientController::class, "index"]);
 Route::post("/ingredients",[IngredientController::class, "store"]);
 Route::put("/ingredients/{id}",[IngredientController::class, "update"]);
 Route::delete("/ingredients/{id}",[IngredientController::class, "destroy"]);
+
+
+Route::get('/prueba', function () {
+    return redirect('https://fakestoreapi.com/products');
+});
+
+Route::get('/prueba2', function () {
+    return ('https://fakestoreapi.com/products');
+});
+
+Route::get('/prueba3', function () {
+    return
+    $response = Http::get('https://fakestoreapi.com/products')
+    ->json();
+});
